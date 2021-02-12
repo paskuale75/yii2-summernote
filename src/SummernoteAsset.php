38b7f19054c9487c469e8810a512e43195e58d8f
simialbi <simi.albi@outlook.com>
2021-02-12 10:28:47 +0100

    removed override config in init function (prevents overriding via configuration file)

--- src/SummernoteAsset.php
+++ src/SummernoteAsset.php
@@ -2,7 +2,6 @@
 
 namespace marqu3s\summernote;
 
-use Yii;
 use yii\web\AssetBundle;
 
 class SummernoteAsset extends AssetBundle
@@ -31,24 +30,4 @@ class SummernoteAsset extends AssetBundle
     public $depends = [
         'yii\bootstrap4\BootstrapPluginAsset',
     ];
-
-    /**
-     * @inheritdoc
-     */
-    public function init()
-    {
-        $postfix = YII_DEBUG ? '' : '.min';
-
-        if (isset(Yii::$app->params['bsVersion']) && Yii::$app->params['bsVersion'] == 4) {
-            $this->depends = ['yii\bootstrap4\BootstrapPluginAsset'];
-            $this->css[] = 'summernote-bs4.css';
-            $this->js[] = 'summernote-bs4' . $postfix . '.js';
-        } else {
-            $this->depends = ['yii\bootstrap\BootstrapPluginAsset'];
-            $this->css[] = 'summernote.css';
-            $this->js[] = 'summernote' . $postfix . '.js';
-        }
-
-        parent::init();
-    }
 }
